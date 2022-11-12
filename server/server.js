9dfef94273e304cbde0d84eead82841126b0f949
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2022-11-12 16:29:06 -0300

    Add Routes & Import to server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const connection = require("./db")
+const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth');
 
 // database connection
 connection();
@@ -10,6 +12,10 @@ connection();
 // middlewares
 app.use(express.json())
 app.use(cors());
+
+//routes
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
